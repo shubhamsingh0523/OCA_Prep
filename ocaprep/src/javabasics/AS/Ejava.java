3139a67531ef546d2487c950770d60a85a7c0777
Drago <Drago@LAPTOP-SKAG2LO8>
2020-09-25 13:24:07 +0530

    new commit

---
+++ ocaprep/src/javabasics/AS/Ejava.java
@@ -0,0 +1,12 @@
+package javabasics.AS;
+
+class Course{
+	String courseName;
+}
+public class Ejava {
+public static void main(String[] args) {
+	Course c=new Course();
+	c.courseName="Java";
+	System.out.println(c.courseName);
+}
+}
